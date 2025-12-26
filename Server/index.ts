e3f8219f1b35c76b8d27878dd3e8fce094ab9e05
TrinidadPatrick <trinidadpatrick019@gmail.com>
2025-12-26 15:02:40 +0800

    fixed failing test and change index initial message

--- Server/index.ts
+++ Server/index.ts
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use('/api', routes)
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
+  res.json({ message: 'Welcome To Swaply Initial Server' });
 });
 
 export default app
